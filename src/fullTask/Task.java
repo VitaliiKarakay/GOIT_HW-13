3c9d6e5095b7ca81e1aff6539268b0979f3e8cdf
IIpizpak <iipizpak@gmail.com>
2021-08-29 21:02:42 +0300

    Added Task-class

---
+++ src/fullTask/Task.java
@@ -0,0 +1,50 @@
+package fullTask;
+
+public class Task {
+    private Long userId;
+    private Long id;
+    private String title;
+    private boolean completed;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Tasks{" +
+                "userID=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
+}
